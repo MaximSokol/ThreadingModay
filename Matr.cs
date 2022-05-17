faf2302636f4c2ebc49a55705a55f7aa5cc78b2c
SokolMaxim <maximsokol2288@gmail.com>
2022-05-17 12:14:16 +0300

    FCommit

---
+++ Matr.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingModay
+{
+    class Matr
+    {
+        public readonly object locker = new object(); // The object of synchronous access
+        //---------------------------------------------
+
+        Random rand;
+        const string litters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        //---------------------------------------------------------------
+
+        public int Count { get; set; }
+        //-------------------------------
+
+        public Matr(int count)
+        {
+            this.Count = count;
+            rand = new Random((int)DateTime.Now.Ticks);
+        }
+        //------------------------
+
+        public char GetChar()
+        {
+            return litters.ToCharArray()[rand.Next(0, 35)];
+        }
+        //------------------------
+
+        public void Move()
+        {
+            int length;
+            int count;
+            while (true)
+            {
+                count = rand.Next(3, 6);
+                length = 0;
+                Thread.Sleep(rand.Next(50, 5000));
+                for(int i = 0; i < 40; i++)
+                {
+                    lock (locker)
+                    {
+                        Console.CursorTop = 0;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        for(int j = 0; j < i; j++)
+                        {
+                            Console.CursorLeft = Count;
+                            Console.WriteLine("█");
+                        }
+                        if (length < count)
+                            length++;
+                        else
+                            if (length == count)
+                            count = 0;
+                        if (39 - i < length)
+                            length--;
+                        Console.CursorTop = i - length + 1;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        for(int z = 0; z < length; z++)
+                        {
+                            Console.CursorLeft = Count;
+                            Console.WriteLine(GetChar());
+                        }
+                        if(length >= 2)
+                        {
+                            Console.CursorLeft = Count;
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(GetChar());
+                        }
+                        if(length >= 1)
+                        {
+                            Console.CursorLeft = Count;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(GetChar());
+                        }
+                        Thread.Sleep(20);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingModay
+{
+    class Program
+    {
+
+        // Task 1
+
+
+        //public static int add;
+
+        //public static void Reverse()
+        //{
+        //    Thread th = new Thread(Reverse);
+        //    add++;
+        //    th.Name = "Second" + add;
+        //    Thread.Sleep(1000);
+        //    th.Start();
+        //    Console.WriteLine(Thread.CurrentThread.Name);
+        //}
+
+
+        // Taks 2
+
+        static void Main(string[] args)
+        {
+            // Task 1.1
+
+
+            //Thread th = new Thread(Reverse) { Name = "first" + add };
+            //th.Start();
+
+
+            // Task 2.1
+
+            Matr mat;
+
+            for (int i = 0; i < 30; i++)
+            {
+                mat = new Matr(i * 2);
+                new Thread(mat.Move).Start();
+            }
+        }
+    }
+}
